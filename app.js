b781eee6af883e01eb1e9ecab960ab6ef9c12321
Ryoko Oka <ryooo.kooo.22@gmail.com>
2021-01-25 23:13:43 -0500

    editing express

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 // copy from "Nodejs"_"about"
-const http = require('http'); // like a PHP require
+const express = require('express'); // like a PHP require
+const path = require('path');
 
-// require is more or less the same as a JS import
-// const hostname = '127.0.0.1'; // this is localhost
+const server = express();
+
+
+// set our views directory
+server.set("views", path.join(__dirname, 'views'));
+// set the static asets director so Express knows where to look
+// for css files, JS files, images et - anything static
+server.use(express.static(path.join(__dirname, "public")));
+
+server.get("/", (req,res) => {
+  console.log('you have hit the home route');
+  res.sendFile('views/index.html');
+})
+
+server.get("/contact", (req, res) => {
+  console.log('hit the contact route');
+  res.sendFile('views/contact.html');
+})
+
 const port = process.env.PORT || 3000; // localhost:3000
-
-const server = http.createServer((req, res) => { // req:request res:response
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Hello World is here');
-});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
